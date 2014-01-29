91d0ef7b5e5dc7d1d87c8ff4b023b1060601a732
pitmax <maxime.pittavino@gmail.com>
2014-01-29 21:26:05 +0100

    Create showscreen.php
    Fichier vide (a remplir donc) avec mes idées pour une interface déporté

---
+++ sound/showscreen.php
@@ -0,0 +1,14 @@
+<?php
+  /*
+  Ce fichier sert à l'affichage
+  Le but est d'avoir un écran a montrer aux joueurs
+  L'avantage d'un fonctionnement en deux écrans et de pouvoir disposer d'une interface de commande et de présentation différentes
+  (un peu comme un écran de jeu)
+  Ce qui a égallement comme avantage de pouvoir disposer d'une interface sur smartphone
+  qui commanderai l'execution des tâches plus lourdes (player par exemple) sur un poste fixe
+  
+  Maintenant, je ne sais pas comment ça peut marcher? Ajax, ça marche comment? Ca vérifie régulièrement un truc et ça met à jour si necessaire?
+  De toute façon, il suffirait d'avoir un fichier "en cours" dans lequel on mettrait les données à afficher (musiques et images)
+  et que l'afficheur irait checker régulièrement
+  */
+?>
